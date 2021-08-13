03a5439d8c95b8ad59e9a7fecdb5ae7c68c92260
webdads2u <webdads2u@gmail.com>
2021-08-13 19:41:30 +0530

    bootstrap theme integration

---
+++ resources/views/frontend/navbar.blade.php
@@ -0,0 +1,54 @@
+<nav class="navbar navbar-expand-md sticky-top addShadow no-radius">
+    <div class="container headnav">
+        <a class="navbar-brand" href="#">
+            <img class="header-logo" src="./images/logo.svg" alt="bumaas">
+        </a>
+        <form class="form-group">
+            <div class="input-group">
+                <input type="text" class="form-control head-search-box"
+                    placeholder="Enter Product, part Number (English Only)">
+                <div class="input-group-btn header-search-wrap">
+                    <button class="btn btn-primary rounded-end" type="submit">
+                        Search
+                    </button>
+                </div>
+            </div>
+        </form>
+        <ul class="navbar-nav d-flex align-items-center justify-content-end">
+            <li class="nav-item">
+                <div class="d-flex align-items-center">
+                    <div>
+                        <button type="button" class="btn btn-link btn-head-icon"><i class="fa fa-phone"
+                                aria-hidden="true"></i></button>
+                    </div>
+                    <div class="header-phone-wrapper">
+                        <div class="head-phone-title">Call Us Now:</div>
+                        <div class="head-phone-number">
+                            0(800) 123-456
+                        </div>
+                    </div>
+                </div>
+            </li>
+            <li class="nav-item">
+                <button type="button" class="btn btn-link btn-head-icon"><i class="fa fa-user-o"
+                        aria-hidden="true"></i></button>
+            </li>
+            <li class="nav-item">
+                <button type="button" class="btn btn-link btn-head-icon"><i class="fa fa-heart-o"
+                        aria-hidden="true"></i></button>
+            </li>
+            <li class="nav-item">
+                <button type="button" class="btn btn-link btn-head-icon"><i class="fa fa-shopping-basket"
+                        aria-hidden="true"></i></button>
+            </li>
+        </ul>
+    </div>
+</nav>
+<!-- <nav style="--bs-breadcrumb-divider: '>';" class="breadcrumb-wrapper" aria-label="breadcrumb"> -->
+<nav class="breadcrumb-wrapper" aria-label="breadcrumb">
+    <ol class="breadcrumb justify-content-center">
+        <li class="breadcrumb-item"><a href="index.php">Home</a></li>
+        <li class="breadcrumb-item active" aria-current="page">Library</li>
+        <li class="breadcrumb-item active">New customer registration</li>
+    </ol>
+</nav>
